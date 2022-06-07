20f465079f076b3940d00bb8590cdaef08a13fbb
margacampos <margacampos@hotmail.es>
2022-06-07 13:08:52 +0100

    Has path function with undirected graph

---
+++ undirected-path.js
@@ -0,0 +1,38 @@
+const edges = [
+    ['i', 'j'],
+    ['k', 'i'],
+    ['m', 'k'],
+    ['k', 'l'],
+    ['o', 'n'],
+]
+
+// build graph from edges 
+
+const buildGraph = (edges) => {
+    let graph = {};
+    for ( let edge of edges ){
+        let [a, b] = edge;
+        if(!(a in graph))graph[a] = [];
+        if(!(b in graph))graph[b] = [];
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+    return graph;
+};
+
+console.log(buildGraph(edges));
+
+// Has path using depth traversal
+
+const depthHasPath = ( graph, start, end, path ) => {
+    if ( start === end ) return true;
+    path.push(start);
+
+    for ( node of graph[start] ){
+        if( !path.includes(node) && depthHasPath( graph, node, end, path ) )return true;
+    }
+
+    return false;
+};
+
+console.log(depthHasPath(buildGraph(edges), 'i', 'o', []));
