21cd9b729d848004ff0c72e57b4bccb853584365
margacampos <margacampos@hotmail.es>
2022-06-07 12:09:33 +0100

    Breadth first traversal functions

---
+++ breadth-first-traversal.js
@@ -0,0 +1,34 @@
+const graph = {
+    a: ['c', 'b'],
+    b: ['d'],
+    c: ['e'],
+    d: ['f'],
+    e: [],
+    f: []
+}
+
+// Iterative:
+const breadthFirstTraversal = ( graph, start ) => {
+    let queue = [ start ];
+    while ( queue.length > 0 ){
+        const current = queue.shift();
+        console.log(current);
+        for ( node of graph[current] ){
+            queue.push(node);
+        }
+    }
+};
+
+// breadthFirstTraversal( graph, 'a' );
+
+// ( Not complete - Does not print start ) Recursive:
+const breadthFirstTraversalRec = ( graph, start ) => {
+    for( neighbor of graph[start] ){
+        console.log(neighbor);
+    }
+    for( neighbor of graph[start] ){
+        breadthFirstTraversalRec( graph, neighbor );
+    }
+};
+
+breadthFirstTraversalRec( graph, 'a' );
